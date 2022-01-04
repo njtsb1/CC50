8a27a7b080939276c0162426a5abb95b22553804
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 21:50:53 -0300

    Update history.php

--- pset7/history.php
+++ pset7/history.php
@@ -8,25 +8,25 @@
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<link rel="stylesheet" type="text/css" href="css/styles.css"/>
-    <title>CC50 Finanças: Histórico de Operações</title>
+    <title>CC50 Finance: Operations History</title>
 </head>
 <body>
 	<div id="top">
-		<a href="index.php"><img alt="CC50 Finanças" src="images/logo.png" style="height: 200px;"></a>
+		<a href="index.php"><img alt="CC50 Finance" src="images/logo.png" style="height: 200px;"></a>
 	</div>
 
 	<nav>
-		<a href="buy.php" title="Comprar ações">Comprar</a><a href="sell.php" title="Vender ações">Vender</a>
-		<a href="history.php" title="Histórico de transações">Histórico</a><a href="quote.php" title="Consultar valor de ação">Cotações</a>
-		<a href="password.php" title="Mude sua senha">Mudar Senha</a><a href="logout.php" title="Fazer logoff">Sair</a>
+		<a href="buy.php" title="Buy shares">Purchase</a><a href="sell.php" title="Sell share">Sell</a>
+		<a href="history.php" title="Transaction history">History</a><a href="quote.php" title="Consult share value">Quotation</a>
+		<a href="password.php" title="Change your password">Change Password</a><a href="logout.php" title="Log off">To go out</a>
 	</nav>
 
 	<div id="middle">
 		<?
 			$query = mysqli_query($connection, "SELECT * FROM history WHERE uid = {$_SESSION["uid"]}");
-			print("<table><tr><td>Ação</td><td>Operação</td><td>Quantidade</td><td>Preço</td><td>Data</td></tr>");
+			print("<table><tr><td>Action</td><td>Operation</td><td>The amount</td><td>Price</td><td>Date</td></tr>");
 			while ($row = mysqli_fetch_array($query)) {
-				$type = ($row["operation"] == "B") ? "Compra" : "Venda";
+				$type = ($row["operation"] == "B") ? "Purchase" : "Sell";
 				print("<tr><td>{$row["symbol"]}</td><td>$type</td><td>{$row["stocks"]}</td><td>{$row["price"]}</td>
 					   <td>{$row["date"]}</td></tr>");
 			}
@@ -34,7 +34,7 @@
 		?>
 	</div>
 	<div id="bottom">
-		<span>© Matheus</span>
+		<span>© njtsb1</span>
 	</div>
 </body>
 </html>
