9d70f3a7639bfc1b6a1429d21c3f50106d9fa480
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 22:40:45 -0300

    Update recover.c

--- pset5/jpg/recover.c
+++ pset5/jpg/recover.c
@@ -1,4 +1,4 @@
-// recover.c	CC50 Pset5	Recupera imagens JPG. Matheus.
+// recover.c	CC50 Pset5	retrieve images JPG. njtsb1.
 
 #include <stdint.h>
 #include <stdio.h>
@@ -8,43 +8,43 @@ uint8_t checkJPG(uint8_t *buffer);
 int
 main(void)
 {
-	// Boas vindas
-	printf("\033[1;30;47mPOWERED BY RAMALHOLIVEIRA\033[0m\nIniciando operação.\n");
+	// Welcome
+	printf("\033[1;30;47mPOWERED BY njtsb1\033[0m\nStarting operation.\n");
 
-	// Abrir arquivo
+	// Open file
 	FILE *fpr = fopen("card.raw", "r");
 	if (fpr == NULL) {
-		printf("\033[31mErro ao abrir o arquivo.\033[0m\n");
+		printf("\033[31mError opening file.\033[0m\n");
 		return 1;
 	}
 
-	// Variáveis
+	//Variables
 	uint8_t buffer[512];
 	int count = 0;
 	char name[8];
-	FILE *saida;
+	FILE *exit;
 
-	// Loop que copia
+	// Loop that copies
 	while (fread(&buffer, 512, 1, fpr) == 1)
 		while (checkJPG(buffer)) {
 			count++;
 			sprintf(name, "%03d.jpg", count);
-			saida = fopen(name, "w");
-			fwrite(&buffer, 512, 1, saida);
-			// Fim do arquivo ou outra assinatura, para
+			exit = fopen(name, "w");
+			fwrite(&buffer, 512, 1, exit);
+			// End of file or other signature, for
 			while (fread(&buffer, 512, 1, fpr) == 1 && !checkJPG(buffer))
-				fwrite(&buffer, 512, 1, saida);
+				fwrite(&buffer, 512, 1, exit);
 		}
 
-	// Isso é tudo, pessoal :)
-	count < 2 ? printf("%d encontrado.\n", count) : printf("Encontrados! %d.\n", count);
+	// That's all, folks :)
+	count < 2 ? printf("%d found.\n", count) : printf("Encontrados! %d.\n", count);
 	fclose(fpr);
-	fclose(saida);
-	printf("Isso é tudo, pessoal.\n");
+	fclose(exit);
+	printf("That's all, folks .\n");
 	return 0;
 }
 
-// Verifica assinatura JPG
+// Check signature JPG
 uint8_t
 checkJPG(uint8_t *buffer)
 {
