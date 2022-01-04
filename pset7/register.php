73dcb2804f8afdadeecb398ae2a6baadcf7a66a7
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 21:14:00 -0300

    Update register.php

--- pset7/register.php
+++ pset7/register.php
@@ -8,27 +8,27 @@
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<link rel="stylesheet" type="text/css" href="css/styles.css"/>
-	<title>CC50 Finanças: Registro</title>
+	<title>CC50 Finance: Registration</title>
 </head>
 <body>
 	<div id="top">
-		<a href="index.php"><img alt="CC50 Finanças" src="images/logo.png" style="height: 200px;"></a>
+		<a href="index.php"><img alt="CC50 Finance" src="images/logo.png" style="height: 200px;"></a>
 	</div>
 
 	<div id="middle">
 		<form action="register2.php" method="post">
 			<table>
 				<tr>
-					<td>Usuário:</td><td><input name="username" type="text"></td>
+					<td>User:</td><td><input name="username" type="text"></td>
 				</tr>
 				<tr>
-					<td>Senha:</td><td><input name="password" type="password"></td>
+					<td>Password:</td><td><input name="password" type="password"></td>
 				</tr>
 				<tr>
-					<td>Repita a senha:</td><td><input name="password2" type="password"></td>
+					<td>Repeat the password:</td><td><input name="password2" type="password"></td>
 				</tr>
 				<tr>
-					<td></td><td><input type="submit" value="Registrar"></td>
+					<td></td><td><input type="submit" value="Register"></td>
 				</tr>
 			</table>
 		</form>
@@ -36,7 +36,7 @@
 	</div>
 
 	<div id="bottom">
-		<span>© Matheus</span>
+		<span>© njtsb1</span>
 	</div>
 </body>
 </html>
