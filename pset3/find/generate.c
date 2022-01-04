1b29144c9025d27b7396d999025a90e57836d4b7
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 23:05:40 -0300

    Update generate.c

--- pset3/find/generate.c
+++ pset3/find/generate.c
@@ -21,26 +21,26 @@
 int
 main(int argc, char *argv[])
 {
-	// Verificação dos argumentos de linha de comando
+	// Checking Command Line Arguments
 	if (argc != 2 && argc != 3)
 	{
 		printf("Usage: %s n [s]\n", argv[0]);
 		return 1;
 	}
 
-	// Transforma o primeiro argumento em INT e armazena em N
+	// Transforms first argument to INT and stores to N
 	int n = atoi(argv[1]);
 
-	// Verifica se são 2 ou 3 argumentos
+	// Check if there are 2 or 3 arguments
 	if (argc == 3)
 		srand((unsigned int) atoi(argv[2]));
 	else
 		srand((unsigned int) time(NULL));
 
-	// Imprime os números pseudo-aleatórios
+	// Print pseudo-random numbers
 	for (int i = 0; i < n; i++)
 		printf("%d\n", rand() % LIMIT);
 
-	// Encerra o programa
+	// Closes the program
 	return 0;
 }
