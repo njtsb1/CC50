3da629f537a075c5f8e05f17bb50a133ab143390
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 21:11:28 -0300

    Update register2.php

--- pset7/register2.php
+++ pset7/register2.php
@@ -3,17 +3,17 @@
 	require_once("includes/common.php");
 
 	if (empty($_POST["username"]) || empty($_POST["password"]))
-		apologize("Preencha todos os campos.");
+		apologize("Fill in all fields.");
 
 	if ($_POST["password"] != $_POST["password2"])
-		apologize("Senhas diferentes.");
+		apologize("Different passwords.");
 
 	// escapar nome de usuário e senha para segurança
 	$username = mysqli_real_escape_string($connection, $_POST["username"]);
 	$password = mysqli_real_escape_string($connection, $_POST["password"]);
 
 	if (!mysqli_query($connection, "INSERT INTO users (username, password, cash) VALUES ('$username', '$password', 10000.0000)"))
-		apologize("Nome de usuário já existe.");
+		apologize("Username already exists.");
 
 	$uid = mysqli_insert_id($connection);
 	$_SESSION["uid"] = $uid;
