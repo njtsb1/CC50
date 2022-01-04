91ca2b4c1af24e620c55699e8d1a274840816f4f
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 22:16:14 -0300

    Update apology.php

--- pset7/includes/apology.php
+++ pset7/includes/apology.php
@@ -4,21 +4,21 @@
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<link rel="stylesheet" type="text/css" href="css/styles.css"/>
-	<title>CC50 Finanças: Ops!</title>
+	<title>CC50 Finance: Oops!</title>
 </head>
 <body>
 	<div id="top">
-		<a href="index.php"><img alt="CC50 Finanças" src="images/logo.png" style="height: 200px;"></a>
+		<a href="index.php"><img alt="CC50 Finance" src="images/logo.png" style="height: 200px;"></a>
 	</div>
 
 	<div id="middle">
-		<h1>Ops!</h1>
+		<h1>Oops!</h1>
 		<h2><?= $message ?></h2>
-		<p><a href="javascript:history.go(-1);">Voltar</a></p>
+		<p><a href="javascript:history.go(-1);">Return</a></p>
 	</div>
 
 	<div id="bottom">
-		<span>© Matheus</span>
+		<span>© njtsb1</span>
 	</div>
 </body>
 </html>
