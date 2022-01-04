498f4921fcf0dd2bbec78079acf2872401f9edfe
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 21:42:40 -0300

    Update index.php

--- pset7/index.php
+++ pset7/index.php
@@ -8,26 +8,26 @@
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<link rel="stylesheet" type="text/css" href="css/styles.css"/>
-    <title>CC50 Finanças: Home</title>
+    <title>CC50 Finance: Home</title>
 </head>
 <body>
 	<div id="top">
-		<a href="index.php"><img alt="CC50 Finanças" src="images/logo.png" style="height: 200px;"></a>
+		<a href="index.php"><img alt="CC50 Finance" src="images/logo.png" style="height: 200px;"></a>
 	</div>
 
 	<nav>
-		<a href="buy.php" title="Comprar ações">Comprar</a><a href="sell.php" title="Vender ações">Vender</a>
-		<a href="history.php" title="Histórico de transações">Histórico</a><a href="quote.php" title="Consultar valor de ação">Cotações</a>
-		<a href="password.php" title="Mude sua senha">Mudar Senha</a><a href="logout.php" title="Fazer logoff">Sair</a>
+		<a href="buy.php" title="Buy shares">Purchase</a><a href="sell.php" title="Sell share">Sell</a>
+		<a href="history.php" title="Transaction history">History</a><a href="quote.php" title="Consult share value">Quotation</a>
+		<a href="password.php" title="Change your password">Change Password</a><a href="logout.php" title="Log off">To go out</a>
 	</nav>
 
 	<div id="middle">
 		<?
  			$query1 = mysqli_query($connection, "SELECT username, cash FROM users WHERE uid = {$_SESSION["uid"]}");
  			$uc = mysqli_fetch_array($query1);
- 			print("<table><tr><td>{$uc[0]}</td><td>Valor disponível: {$uc[1]}.</td></tr></table><br/>");
+ 			print("<table><tr><td>{$uc[0]}</td><td>Available value: {$uc[1]}.</td></tr></table><br/>");
  			$query2 = mysqli_query($connection, "SELECT symbol, shares FROM wallet WHERE uid = {$_SESSION["uid"]}");
- 			print("<table><tr><td>Nome</td><td>Símbolo</td><td>Preço</td><td>+Baixo</td><td>+Alto</td><td>Quantidade</td></tr>");
+ 			print("<table><tr><td>Name</td><td>Symbol</td><td>Price</td><td>+Low</td><td>+High</td><td>The amount</td></tr>");
  			while ($row = mysqli_fetch_array($query2)) {
  				$s = lookup($row["symbol"]);
  				print("<tr><td>{$s->name}</td><td>{$s->symbol}</td><td>{$s->price}</td><td>{$s->low}</td><td>{$s->high}</td>
@@ -38,7 +38,7 @@
 	</div>
 
 	<div id="bottom">
-		<span>© Matheus</span>
+		<span>© njtsb1</span>
 	</div>
 </body>
 </html>
