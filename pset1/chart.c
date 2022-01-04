2cff58670a1f92498105284dd15ec3d7d67d8530
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 23:27:25 -0300

    Update chart.c

--- pset1/chart.c
+++ pset1/chart.c
@@ -1,49 +1,49 @@
 int
 main(void)
 {
-	int hm, hh, mh, mm;
+	int mw, mm, wm, ww;
 
 	do {
-		printf("\nQuantos homens procuraram mulheres? ");
-		hm = GetInt();
-	} while (hm < 0);
+		printf("\nHow many men looked for women? ");
+		mw = GetInt();
+	} while (mw < 0);
 
 	do {
-		printf("\nQuantos homens procuraram homens? ");
-		hh = GetInt();
-	} while (hh < 0);
-
-	do {
-		printf("\nQuantas mulheres procuraram homens? ");
-		mh = GetInt();
-	} while (mh < 0);
-
-	do {
-		printf("\nQuantas mulheres procuraram mulheres? ");
+		printf("\nHow many men looked for men? ");
 		mm = GetInt();
 	} while (mm < 0);
 
-	int total = hm + hh + mh + mm;
-	printf("\nTotal: %d. Gráfico de buscas:\n", total);
+	do {
+		printf("\nHow many women looked for men? ");
+		wm = GetInt();
+	} while (wm < 0);
 
-	int cerquilha = 80*hm/total, i = 100*hm/total;
-	printf("\nH procurando M - %d%%\n", i);
-	for (int i = 0; i < cerquilha; i++)
+	do {
+		printf("\nHow many women looked for women? ");
+		ww = GetInt();
+	} while (ww < 0);
+
+	int total = mw + mm + wm + ww;
+	printf("\nTotal: %d. Search chart:\n", total);
+
+	int tether = 80*mw/total, i = 100*mw/total;
+	printf("\nM searching W - %d%%\n", i);
+	for (int i = 0; i < tether; i++)
 		printf("#");
 
-	cerquilha = 80*hh/total, i = 100*hh/total;
-	printf("\nH procurando H - %d%%\n", i);
-	for (int i = 0; i < cerquilha; i++)
+	tether = 80*mm/total, i = 100*mm/total;
+	printf("\nM searching M - %d%%\n", i);
+	for (int i = 0; i < tether; i++)
 		printf("#");
 
-	cerquilha = 80*mh/total, i = 100*mh/total;
-	printf("\nM procurando H - %d%%\n", i);
-	for (int i = 0; i < cerquilha; i++)
+	tether = 80*wm/total, i = 100*wm/total;
+	printf("\nW searching M - %d%%\n", i);
+	for (int i = 0; i < tether; i++)
 		printf("#");
 
-	cerquilha = 80*mm/total, i = 100*mm/total;
-	printf("\nM procurando M - %d%%\n", i);
-	for (int i = 0; i < cerquilha; i++)
+	tether = 80*ww/total, i = 100*ww/total;
+	printf("\nW searching W - %d%%\n", i);
+	for (int i = 0; i < tether; i++)
 		printf("#");
 
 	printf("\n\n");
