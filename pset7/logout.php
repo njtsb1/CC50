b26d529f0510a809d8c1b0b186188c7201ff7448
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 21:32:13 -0300

    Update logout.php

--- pset7/logout.php
+++ pset7/logout.php
@@ -11,20 +11,20 @@
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<link rel="stylesheet" type="text/css" href="css/styles.css"/>
-	<title>CC50 Finanças: Log out</title>
+	<title>CC50 Finance: Log out</title>
 </head>
 <body>
 	<div id="top">
-		<a href="index.php"><img alt="CC50 Finanças" src="images/logo.png" style="height: 200px;"></a>
+		<a href="index.php"><img alt="CC50 Finance" src="images/logo.png" style="height: 200px;"></a>
 	</div>
 
 	<div id="middle">
 		<p>kthxbai</p>
-		<p>faça o <a href="login.php">login</a> de novo</p>
+		<p>machete <a href="login.php">login</a> again/p>
 	</div>
 
 	<div id="bottom">
-		<span>© Matheus</span>
+		<span>© njtsb1</span>
 	</div>
 </body>
 </html>
