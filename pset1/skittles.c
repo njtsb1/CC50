23d4a56e3fc9e29152918da568224bf975217fb9
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-04 07:07:14 -0300

    Update skittles.c

--- pset1/skittles.c
+++ pset1/skittles.c
@@ -1,3 +1,16 @@
+//Let's start programming?!
+//So one of the best things about Maxwell Dworkin (the Computer Science building of
+//Harvard) is the free candy machine in the lounge.
+//There are a lot of Skittles (bullets) and M&M's in this
+//machine. Want to guess how many Skittles are there?
+//Implement, in a file called skittles.c, a program
+//which first chooses a random (pseudo) number between 0 and 1023 and then asks you (the human) 
+//to guess what that value is.14 The program should keep asking you to guess.
+//until you guess the right amount, then he should thank you for playing.
+//Where to start? Allow us to give you some pieces of the puzzle.
+//To generate a random number, you can use a function called rand.
+
+
 int
 main(void)
 {
