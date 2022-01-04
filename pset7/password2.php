e643de00fdf205f13f098b35f1a328142fda11bf
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 21:23:59 -0300

    Update password2.php

--- pset7/password2.php
+++ pset7/password2.php
@@ -3,18 +3,18 @@
 	require_once("includes/common.php");
 
 	if ($_POST["pass2"] != $_POST["pass3"])
-		apologize("As senhas escolhidas não coincidem.");
+		apologize("The chosen passwords do not match.");
 
 	$query = mysqli_query($connection, "SELECT password FROM users WHERE uid = {$_SESSION["uid"]}");
 	$old_password = mysqli_fetch_array($query);
 
 	if ($_POST["pass1"] != $old_password[0])
-		apologize("Senha antiga está errada.");
+		apologize("Old password is wrong.");
 
 	$new_password = mysqli_real_escape_string($connection, $_POST["pass2"]);
 
 	if (!mysqli_query($connection, "UPDATE users SET password = '$new_password' WHERE uid = {$_SESSION["uid"]}"))
-		apologize("Não foi possível alterar sua senha.");
+		apologize("Unable to change your password.");
 
-	apologize("Senha alterada com sucesso.");
+	apologize("Password changed successfully.");
 ?>
