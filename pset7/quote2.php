3e48e2c3086b3c75ec12fb72c097b9e8b2dbe3fc
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 21:16:18 -0300

    Update quote2.php

--- pset7/quote2.php
+++ pset7/quote2.php
@@ -2,7 +2,7 @@
 	require_once("includes/common.php");
 	$s = lookup($_POST["symbol"]);
 	if (!$s)
-		apologize("Símbolo inválido.");
+		apologize("Invalid Symbol.");
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,20 +10,20 @@
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<link rel="stylesheet" type="text/css" href="css/styles.css"/>
-	<title>CC50 Finanças: Valor da cotação</title>
+	<title>CC50 Finance: Quotation Value</title>
 </head>
 <body>
 	<div id="top">
-		<a href="index.php"><img alt="CC50 Finanças" src="images/logo.png" style="height: 200px;"></a>
+		<a href="index.php"><img alt="CC50 Finance" src="images/logo.png" style="height: 200px;"></a>
 	</div>
 
 	<div id="middle">
-		<p><div style="text-align: center">A ação <?= $s->name ?> atualmente custa $<?= $s->price ?>.</div></p>
-		<p><a href="javascript:history.go(-1);">Voltar</a></p>
+		<p><div style="text-align: center">The action <?= $s->name ?> currently costs $<?= $s->price ?>.</div></p>
+		<p><a href="javascript:history.go(-1);">Return</a></p>
 	</div>
 
 	<div id="bottom">
-		<span>© Matheus</span>
+		<span>© njtsb1</span>
 	</div>
 </body>
 </html>
