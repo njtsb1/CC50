91583aa7dd9ebeb632a12dd60fb1001a82aa26f6
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 21:52:56 -0300

    Update buy2.php

--- pset7/buy2.php
+++ pset7/buy2.php
@@ -6,25 +6,25 @@
 	$shares = mysqli_real_escape_string($connection, $_POST["ssha"]);
 
 	if (!preg_match("/^\d+$/", $shares))
-		apologize("Você pode comprar somente com valores inteiro positivo.");
+		apologize("You can only buy with positive integer values.");
 
 	$stock = lookup($symbol);
 	$query = mysqli_query($connection, "SELECT cash FROM users WHERE uid = {$_SESSION["uid"]}");
 	$slices = mysqli_fetch_array($query);
 
 	if ($shares > $slices["cash"])
-		apologize("Você não têm dinheiro suficiente para comprar.");
+		apologize("You don't have enough money to buy.");
 
 	if (!mysqli_query($connection, "INSERT INTO wallet (uid, symbol, shares) VALUES ({$_SESSION["uid"]}, '$stock->symbol', $shares)
 									ON DUPLICATE KEY UPDATE shares = shares + $shares"))
-		apologize("Falha ao comprar a ação \"{$stock->name}\".");
+		apologize("Failed to buy the share \"{$stock->name}\".");
 
 	if (!mysqli_query($connection, "UPDATE users SET cash = cash - $stock->price WHERE uid = {$_SESSION["uid"]}"))
-		apologize("Falha ao remover o dinheiro da sua carteira.");
+		apologize("Failed to remove money from your wallet.");
 
 	if (!mysqli_query($connection, "INSERT INTO history (uid, operation, symbol, stocks, price)
 									VALUES ({$_SESSION["uid"]}, 'B', '$stock->symbol', $shares, $stock->price)"))
-		apologize("Falha ao armazenar informações no histórico.");
+		apologize("Failed to store information in history.");
 
 	redirect("buy.php");
 ?>
