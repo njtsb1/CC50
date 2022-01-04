8b1fb6df4b97602d8e090ce2edc08ea5d95af746
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 21:30:12 -0300

    Update password.php

--- pset7/password.php
+++ pset7/password.php
@@ -8,30 +8,30 @@
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<link rel="stylesheet" type="text/css" href="css/styles.css"/>
-    <title>CC50 Finanças: Mudar Senha</title>
+    <title>CC50 Finance: Change Password</title>
 </head>
 <body>
 	<div id="top">
-		<a href="index.php"><img alt="CC50 Finanças" src="images/logo.png" style="height: 200px;"></a>
+		<a href="index.php"><img alt="CC50 Finance" src="images/logo.png" style="height: 200px;"></a>
 	</div>
 
 	<nav>
-		<a href="buy.php" title="Comprar ações">Comprar</a><a href="sell.php" title="Vender ações">Vender</a>
-		<a href="history.php" title="Histórico de transações">Histórico</a><a href="quote.php" title="Consultar valor de ação">Cotações</a>
-		<a href="password.php" title="Mude sua senha">Mudar Senha</a><a href="logout.php" title="Fazer logoff">Sair</a>
+		<a href="buy.php" title="Buy shares">Purchase</a><a href="sell.php" title="Sell shares">Sell</a>
+		<a href="history.php" title="Transaction history">History</a><a href="quote.php" title="Consult share value">Quotation</a>
+		<a href="password.php" title="Change your password">Change Password</a><a href="logout.php" title="Log off">To go out</a>
 	</nav>
 
 	<div id="middle">
 		<form action="password2.php" method="post">
-			Senha antiga: <input type="password" name="pass1"/><br/>
-			Nova senha: <input type="password" name="pass2"/><br/>
-			Repita a senha nova: <input type="password" name="pass3"/><br/>
-			<input type="submit" value="Trocar"/>
+			Old Password: <input type="password" name="pass1"/><br/>
+			New Password: <input type="password" name="pass2"/><br/>
+			Repeat new password: <input type="password" name="pass3"/><br/>
+			<input type="submit" value="To exchange"/>
 		</form>
 	</div>
 
 	<div id="bottom">
-		<span>© Matheus</span>
+		<span>© njtsb1</span>
 	</div>
 </body>
 </html>
