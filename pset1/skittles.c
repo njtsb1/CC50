63eb55f4aec31f42dbcfb277edfc09f215a9e985
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 23:16:55 -0300

    Update skittles.c

--- pset1/skittles.c
+++ pset1/skittles.c
@@ -4,19 +4,19 @@ main(void)
 	// seed do PRNG
 	srand(time());
 
-	// escolhe número pseudo-aleatório entre [0, 1023]
+	// choose pseudo-random number among [0, 1023]
 	int skittles = rand() % 1024, i = 0;
 
-	printf("Olá! Eu sou uma máquina de balas falante! Adivinhe quantos Skittles têm dentro de mim. Dica: estou pensando em um número entre 0 e 1023. Qual é ele?\nDigite um número: ");
+	printf("Hi! I am a talking bullet machine! Guess how many Skittles I have inside me. Hint: I'm thinking of a number between 0 and 1023. What is it?\nType a number: ");
 
 	do {
 		i = GetInt();
 		if (i < skittles)
-			printf("Haha! Tenho muito mais Skittles do que isso. Tente novamente.\nDigite um número: ");
+			printf("Haha! I have a lot more Skittles than that. Try again.\nEnter a number: ");
 		else if (i > skittles)
-			printf("Não tente ser difícil... Adivinhe novamente.\nDigite um número: ");
+			printf("Don't try to be difficult... Guess again.\nEnter a number: ");
 		else {
-			printf("\nVocê está certo! Nom nom nom nom.\n");
+			printf("\nYou're right! Nom nom nom nom.\n");
 			break;
 		}
 	} while (i != skittles);
