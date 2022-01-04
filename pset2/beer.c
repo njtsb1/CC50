1a86c10bdcdb297be943d699bea6c23f4e1d8ef4
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 23:12:35 -0300

    Update beer.c

--- pset2/beer.c
+++ pset2/beer.c
@@ -3,15 +3,15 @@ main(void)
 {
 	int i = 10;
 	while (i > 0) {
-		printf("%d garrafas de cerveja no muro, bebo uma, jogo no lixo, ", i);
+		printf("%d beer bottles on the wall, drink one, throw it in the trash, ", i);
 		i--;
 		if (i == 1) {
-			printf("%d garrafa no muro...\n", i);
+			printf("%d bottle on the wall...\n", i);
 			sleep(1);
-			printf("1 garrafa de cerveja no muro, bebo uma, jogo no lixo, nenhuma garrafa no muro...\n");
+			printf("1 beer bottle on the wall, drink one, throw it in the trash, no bottle on the wall...\n");
 			return 0;
 		}
-		printf("%d garrafas no muro...\n", i);
+		printf("%d bottles on the wall...\n", i);
 		sleep(1);
 	}
 }
