5d51c6fe40d1c9e619e45273d0f64438ffda65cf
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 21:59:38 -0300

    Update buy.php

--- pset7/buy.php
+++ pset7/buy.php
@@ -8,34 +8,34 @@
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<link rel="stylesheet" type="text/css" href="css/styles.css"/>
-    <title>CC50 Finanças: Comprar Ações</title>
+    <title>CC50 Finance: Buy Shares</title>
 </head>
 <body>
 	<div id="top">
-		<a href="index.php"><img alt="CC50 Finanças" src="images/logo.png" style="height: 200px;"></a>
+		<a href="index.php"><img alt="CC50 Finance" src="images/logo.png" style="height: 200px;"></a>
 	</div>
 
 	<nav>
-		<a href="buy.php" title="Comprar ações">Comprar</a><a href="sell.php" title="Vender ações">Vender</a>
-		<a href="history.php" title="Histórico de transações">Histórico</a><a href="quote.php" title="Consultar valor de ação">Cotações</a>
-		<a href="password.php" title="Mude sua senha">Mudar Senha</a><a href="logout.php" title="Fazer logoff">Sair</a>
+		<a href="buy.php" title="Buy Shares">Purchase</a><a href="sell.php" title="Sell share">Sell</a>
+		<a href="history.php" title="Transation history">History</a><a href="quote.php" title="Consult share value">Quotation</a>
+		<a href="password.php" title="Change your password">Change password</a><a href="logout.php" title="Log off">To go out</a>
 	</nav>
 
 	<div id="middle">
 		<?
 			$query = mysqli_query($connection, "SELECT username, cash FROM users WHERE uid = {$_SESSION["uid"]}");
 			$uc = mysqli_fetch_array($query);
-			print("<table><tr><td>{$uc[0]}</td><td>Valor disponível: {$uc[1]}.</td></tr></table>");
+			print("<table><tr><td>{$uc[0]}</td><td>Available value: {$uc[1]}.</td></tr></table>");
 		?>
-		<p>Comprar Ações</p>
+		<p>Buy Shares</p>
 		<form action="buy2.php" method="post">
-			Símbolo: <input type="text" name="ssym"/><br/>
-			Quantidade: <input type="number" name="ssha"><br/>
-			<input type="submit" value="Comprar"/>
+			Symbol: <input type="text" name="ssym"/><br/>			
+			The amount: <input type="number" name="ssha"><br/>
+			<input type="submit" value="Purchase"/>
 		</form>
 	</div>
 	<div id="bottom">
-		<span>© Matheus</span>
+		<span>© njtsb1</span>
 	</div>
 </body>
 </html>
