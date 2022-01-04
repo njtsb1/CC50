16c4314411d5e1f0c74287b64a2dd8da24185cd8
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 21:22:29 -0300

    Update quote.php

--- pset7/quote.php
+++ pset7/quote.php
@@ -7,31 +7,31 @@
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<link rel="stylesheet" type="text/css" href="css/styles.css"/>
-	<title>CC50 Finanças: Cotação</title>
+	<title>CC50 Finance: Quotation</title>
 </head>
 <body>
 	<div id="top">
-		<a href="index.php"><img alt="CC50 Finanças" src="images/logo.png" style="height: 200px;"></a>
+		<a href="index.php"><img alt="CC50 Finance" src="images/logo.png" style="height: 200px;"></a>
 	</div>
 
 	<nav>
-		<a href="buy.php" title="Comprar ações">Comprar</a><a href="sell.php" title="Vender ações">Vender</a>
-		<a href="history.php" title="Histórico de transações">Histórico</a><a href="quote.php" title="Consultar valor de ação">Cotações</a>
-		<a href="password.php" title="Mude sua senha">Mudar Senha</a><a href="logout.php" title="Fazer logoff">Sair</a>
+		<a href="buy.php" title="Buy shares">Purchase</a><a href="sell.php" title="Sell shares">Sell</a>
+		<a href="history.php" title="Transaction history">Historic</a><a href="quote.php" title="Consult share value">Quotation</a>
+		<a href="password.php" title="Change your password">Change Password</a><a href="logout.php" title="Log off">To go out</a>
 	</nav>
 
 	<div id="middle">
 		<form action="quote2.php" method="post">
 			<table>
-				<tr><td>Símbolo de uma ação:</td></tr>
+				<tr><td>Symbol of an action:</td></tr>
 				<tr><td><input name="symbol" type="text"></td></tr>
-				<tr><td><input type="submit" value="Consultar"></td></tr>
+				<tr><td><input type="submit" value="Consult"></td></tr>
 			</table>
 		</form>
 	</div>
 
 	<div id="bottom">
-		<span>© Matheus</span>
+		<span>© njtsb1</span>
 	</div>
 </body>
 </html>
