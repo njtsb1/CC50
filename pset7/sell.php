a4e54438695c9287a4de7b3a808d76c42409cf9b
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2022-01-03 21:09:49 -0300

    Update sell.php

--- pset7/sell.php
+++ pset7/sell.php
@@ -8,7 +8,7 @@
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<link rel="stylesheet" type="text/css" href="css/styles.css"/>
-    <title>CC50 Finanças: Vender Ações</title>
+    <title>CC50 Finance: Selling Stocks</title>
 </head>
 <body>
 	<div id="top">
@@ -16,9 +16,9 @@
 	</div>
 
 	<nav>
-		<a href="buy.php" title="Comprar ações">Comprar</a><a href="sell.php" title="Vender ações">Vender</a>
-		<a href="history.php" title="Histórico de transações">Histórico</a><a href="quote.php" title="Consultar valor de ação">Cotações</a>
-		<a href="password.php" title="Mude sua senha">Mudar Senha</a><a href="logout.php" title="Fazer logoff">Sair</a>
+		<a href="buy.php" title="Buy shares">Purchase</a><a href="sell.php" title="Sell shares">Sell</a>
+		<a href="history.php" title="Transaction history">History</a><a href="quote.php" title="Consult share value">Cotações</a>
+		<a href="password.php" title="Change your password">Change Password</a><a href="logout.php" title="Log off">To go out</a>
 	</nav>
 
 	<div id="middle">
@@ -27,7 +27,7 @@
  			$uc = mysqli_fetch_array($query1);
  			print("<table><tr><td>{$uc[0]}</td><td>Valor disponível: {$uc[1]}.</td></tr></table><br/>");
  			$query2 = mysqli_query($connection, "SELECT symbol, shares FROM wallet WHERE uid = {$_SESSION["uid"]}");
- 			print("<table><tr><td>Nome</td><td>Símbolo</td><td>Preço</td><td>+Baixo</td><td>+Alto</td><td>Quantidade</td></tr>");
+ 			print("<table><tr><td>Nome</td><td>Symbol</td><td>Preço</td><td>+Baixo</td><td>+Alto</td><td>Quantidade</td></tr>");
  			while ($row = mysqli_fetch_array($query2)) {
  				$s = lookup($row["symbol"]);
  				print("<tr><td>{$s->name}</td><td>{$s->symbol}</td><td>{$s->price}</td><td>{$s->low}</td><td>{$s->high}</td>
@@ -35,16 +35,16 @@
  			}
  			print("</table>");
  		?>
-		<p>Vender Ações</p>
+		<p>Sell shares</p>
  		<form action="sell2.php" method="post">
- 			Símbolo: <input type="text" name="ssym"/><br/>
- 			Quantidade: <input type="number" name="ssha"><br/>
+ 			Symbol: <input type="text" name="ssym"/><br/>
+ 			The amount: <input type="number" name="ssha"><br/>
  			<input type="submit" value="Vender"/>
  		</form>
  	</div>
 
 	<div id="bottom">
-		<span>© Matheus</span>
+		<span>© njtsb1</span>
 	</div>
 </body>
 </html>
